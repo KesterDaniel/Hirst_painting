918de39268094546175f44ad6ac2faeab1c1651f
KesterDaniel <kesterdan17@gmail.com>
2023-01-18 17:07:07 +0100

    extracting rgb values from an image

--- main.py
+++ main.py
@@ -1,1 +1,17 @@
 # Hirst painting
+import colorgram
+
+
+colors = colorgram.extract("image.jpg", 30)
+
+
+for color in colors:
+    new_color = color.rgb
+    red = new_color.r
+    green = new_color.g
+    blue = new_color.b
+
+    color_list.append((red, green, blue))
+
+
+print(color_list)
